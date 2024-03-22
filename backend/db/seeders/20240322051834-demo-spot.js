623dcd86881e3a12a54693c97792f33cc05a924e
Derek Lin <12linderek@gmail.com>
2024-03-22 15:37:31 -0700

    successfully seeded one demo

---
+++ backend/db/seeders/20240322051834-demo-spot.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { Spot } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await Spot.bulkCreate([
+      {
+        ownerId: 1,
+        address: '4 Bull Run',
+        city: 'Irvine',
+        state: 'CA',
+        country: 'USA',
+        lat: 50.96235,
+        lng: 49.26534,
+        name: 'Childhood Home',
+        description: 'My childhood home growing up.',
+        price: 100.00
+      }
+    ]
+    )
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Spots';
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
