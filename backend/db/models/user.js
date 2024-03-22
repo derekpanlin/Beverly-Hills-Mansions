0fe6345173c84c194ce19bb50ec7b52cae3ebc72
Derek Lin <12linderek@gmail.com>
2024-03-21 22:14:21 -0700

    added static association of User to Spots via hasMany

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -9,7 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-
+      User.hasMany(models.Spot, {
+        foreignKey: 'ownerId'
+      })
     }
   }
   User.init({
