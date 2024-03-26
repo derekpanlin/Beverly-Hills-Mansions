cf271ba5cbae92d1bffe7b15793788bdb942bdd0
Derek Lin <12linderek@gmail.com>
2024-03-25 20:54:02 -0700

    added review model/migrations

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Booking.belongsTo(models.User, {
-        foreignKey: 'userId',
+        foreignKey: 'userId'
       });
 
       Booking.belongsTo(models.Spot, {
