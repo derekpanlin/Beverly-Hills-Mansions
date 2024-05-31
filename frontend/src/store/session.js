fba35db0886cd8855e5de8aeddf5126cbcd4bfd4
Derek Lin <12linderek@gmail.com>
2024-05-30 18:20:55 -0700

    added LoginFormPage.jsx and css files

---
+++ frontend/src/store/session.js
@@ -0,0 +1,48 @@
+import { csrfFetch } from './csrf'
+
+
+// Action creators
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user
+    };
+};
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER
+    };
+};
+
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch("/api/session", {
+        method: "POST",
+        body: JSON.stringify({
+            credential,
+            password
+        })
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER:
+            return { ...state, user: action.payload };
+        case REMOVE_USER:
+            return { ...state, user: null };
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
