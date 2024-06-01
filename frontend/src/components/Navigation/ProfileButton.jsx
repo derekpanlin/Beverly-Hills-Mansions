764df4ec03bfa72643c081e370b612d67637303f
Derek Lin <12linderek@gmail.com>
2024-06-01 11:00:15 -0700

    added toggle functionality to show/hide profile info

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,11 +1,55 @@
-import { RxAccessibility } from "react-icons/rx";
+import { useState, useEffect, useRef } from "react";
+import { useDispatch } from 'react-redux';
+import { RxAvatar } from "react-icons/rx";
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
 
 
-function ProfileButton() {
+    const toggleMenu = (e) => {
+        e.stopPropagation();
+        setShowMenu(!showMenu);
+    }
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (ulRef.current && !ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener("click", closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    const profileDropdownClass = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+
     return (
-        <button class="profile-button">
-            <RxAccessibility size={40} />
-        </button>
+        <div className="profile-button-container">
+            <button onClick={toggleMenu}>
+                <RxAvatar size={40} />
+            </button>
+            <ul className={profileDropdownClass} ref={ulRef}>
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Log Out</button>
+                </li>
+            </ul>
+        </div>
     );
 }
 
