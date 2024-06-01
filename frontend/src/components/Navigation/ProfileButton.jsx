4a352efb771694974bb8de81936be9045dd724fc
Derek Lin <12linderek@gmail.com>
2024-06-01 13:28:38 -0700

    added the login and sign up to be part of the avatar drop down

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import { RxAvatar } from "react-icons/rx";
 import * as sessionActions from '../../store/session';
+import OpenModalButton from "../OpenModalButton/OpenModalButton";
+import LoginFormModal from "../LoginFormModal/LoginFormModal";
+import SignupFormModal from "../SignUpFormModal/SignupFormModal";
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
@@ -42,18 +45,35 @@ function ProfileButton({ user }) {
                 <RxAvatar size={40} />
             </button>
             <ul className={profileDropdownClass} ref={ulRef}>
-                <li>{user.username}</li>
-                <li>{user.firstName} {user.lastName}</li>
-                <li>{user.email}</li>
-                <li>
-                    <button onClick={logout}>Log Out</button>
-                </li>
+                {user ? (
+                    <>
+                        <li>{user.username}</li>
+                        <li>{user.firstName} {user.lastName}</li>
+                        <li>{user.email}</li>
+                        <li>
+                            <button onClick={logout}>Log Out</button>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <OpenModalButton
+                                buttonText="Log In"
+                                modalComponent={<LoginFormModal />}
+                            />
+                        </li>
+                        <li>
+                            <OpenModalButton
+                                buttonText="Sign Up"
+                                modalComponent={<SignupFormModal />}
+                            />
+                        </li>
+                    </>
+                )}
             </ul>
         </div>
     );
 }
-
-
 
 
 export default ProfileButton;
