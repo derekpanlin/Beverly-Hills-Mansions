af09ac6528d2913e68e3d7f7bdeceb72fd3a8aa6
Derek Lin <12linderek@gmail.com>
2024-06-04 20:55:20 -0700

    changed max width of grid container to only allow 4 columns at most

--- frontend/src/components/Spots/Spots.jsx
+++ frontend/src/components/Spots/Spots.jsx
@@ -22,9 +22,12 @@ const SpotsIndex = () => {
                             <img src={spot.previewImage} />
                         </div>
                         <div className='spot-details'>
-                            <div className='spot-name'>{spot.name}</div>
-                            <div className='spot-location'>{spot.city}, {spot.state}</div>
-                            <div className='spot-rating'>⭐ {spot.avgRating} </div>
+                            <h3 className='spot-name'>{spot.name}</h3>
+                            <div className='spot-location-rating'>
+                                <div className='spot-location'>{spot.city}, {spot.state}</div>
+                                <div className='spot-rating'>⭐ {spot.avgRating} </div>
+                            </div>
+
                             <div className="spot-price">${spot.price} / night</div>
                         </div>
                     </div>
