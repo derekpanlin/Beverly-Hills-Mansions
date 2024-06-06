747a7838cf86baf58c1a5c41e72434f78c0c61a7
Derek Lin <12linderek@gmail.com>
2024-06-05 22:02:26 -0700

    added spot image

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -15,8 +15,20 @@ function SpotDetails() {
     useEffect(() => {
         dispatch(getSpotDetails(spotId));
     }, [dispatch, spotId])
+
     return (
-        <h1>Spot Details</h1>
+        <div className='spot-details-container'>
+            <div className='spot-header'>
+                <h2>{spotDetails.name}</h2>
+                <h3>{spotDetails.city}, {spotDetails.state}, {spotDetails.country}</h3>
+            </div>
+            <div className='spot-images-container'>
+                <div className='main-image'>
+                    <img src={spotDetails.SpotImages[0].url} />
+                </div>
+
+            </div>
+        </div>
     );
 }
 
