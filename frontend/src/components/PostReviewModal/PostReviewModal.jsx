464b8e5111b3b059f7b4f7eeedeab4313f463225
Derek Lin <12linderek@gmail.com>
2024-06-12 21:42:16 -0700

    disabled submission button if less than 10 characters and no stars

--- frontend/src/components/PostReviewModal/PostReviewModal.jsx
+++ frontend/src/components/PostReviewModal/PostReviewModal.jsx
@@ -2,18 +2,37 @@ import { useState } from "react";
 import './PostReviewModal.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { useModal } from "../../context/Modal";
 
 function PostReviewModal() {
     const [review, setReview] = useState('');
     const [stars, setStars] = useState(0);
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const { setModalContent, closeModal } = useModal();
 
     const handleStarClick = (index) => {
-        setStars(index + 1); // Increment index by 1 to get the star rating (1-indexed)
+        setStars(index + 1);
+        updateButtonState(review, index + 1);
+
     }
 
-    // Number of stars to render (adjust as needed)
+    const handleTextArea = (e) => {
+        const text = e.target.value;
+        setReview(text);
+        updateButtonState(text, stars);
+    }
+
+    const updateButtonState = (text, rating) => {
+        setButtonDisabled(text.length < 10 && rating < 1)
+    }
+
+    const handleSubmit = () => {
+        // Submission
+        closeModal();
+    }
+
     const starCount = 5;
-    // Array to iterate over for rendering stars
+
     const starArray = Array.from({ length: starCount }, (_, index) => index);
 
     return (
@@ -34,7 +53,12 @@ function PostReviewModal() {
                     />
                 ))}
             </div>
-            <button>Submit your review</button>
+            <button
+                onClick={handleSubmit}
+                disabled={buttonDisabled}
+            >
+                Submit your review
+            </button>
         </div>
     );
 }
